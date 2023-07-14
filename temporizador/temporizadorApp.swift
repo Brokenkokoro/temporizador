d0daec7881177b103cdecf1baa20417703b5d21b
Brokenkokoro <minichiny_7@hotmail.com>
2023-07-14 13:58:09 -0600

    Initial Commit

---
+++ temporizador/temporizadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  temporizadorApp.swift
+//  temporizador
+//
+//  Created by Angel Contreras Medina on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct temporizadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
